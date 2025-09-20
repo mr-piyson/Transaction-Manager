9361941086fbb49239971d3634677828b1b36378
mr-piyson <mun.ateeq@gmail.com>
2025-09-20 21:49:47 +0300

    optimized log - env.ts

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 import { createEnv } from "@t3-oss/env-core";
+import { log } from "./log";
 
 export const env = createEnv({
   server: {
@@ -7,4 +8,21 @@ export const env = createEnv({
     SECRET_KEY: z.string(),
   },
   runtimeEnv: process.env,
+  onValidationError: (issues) => {
+    console.error(`❌ Invalid environment variables:`);
+    issues.forEach((issue) => {
+      log([
+        { message: issue.path, color: "blue" },
+        { message: " → ", color: "reset" },
+        { message: issue.message, color: "red" },
+      ]);
+    });
+
+    // throw error with out log the zod runtime error log
+    process.exit(1);
+  },
+  onInvalidAccess(variable) {
+    console.error(`❌ Invalid access to environment variable: ${variable}`);
+    process.exit(1);
+  },
 });
