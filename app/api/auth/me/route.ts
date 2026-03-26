6c86407cb5db5faea19cd6feeebd7f1244ee9719
mr-piyson <mun.ateeq@gmail.com>
2026-03-26 16:38:35 +0300

    remove unused deps

---
+++ app/api/auth/me/route.ts
@@ -0,0 +1,17 @@
+import { getCurrentUser } from '@/server/auth';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest, ctx: RouteContext<'/api/auth/me'>) {
+  try {
+    const user = await getCurrentUser();
+
+    if (user === null) {
+      return NextResponse.json({ error: 'User not found' }, { status: 404 });
+    }
+
+    return NextResponse.json(user);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  }
+}
