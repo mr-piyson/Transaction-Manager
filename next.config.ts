d16e150e3cece4bab7ae0255e6d5e7117a1e1c64
mr-piyson <mun.ateeq@gmail.com>
2026-07-17 14:52:28 +0300

    refactor: update dev origin, include build step in update script, and implement polyfilled ID generation for supplier items

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   // typedRoutes: true,
   /* config options here */
-  allowedDevOrigins: ['172.18.12.71'],
+  allowedDevOrigins: ['*.*.*.*'],
 };
 
 export default nextConfig;
