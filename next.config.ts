c722fffb055219449f0b77e364e367bf007be4e1
mr-piyson <mun.ateeq@gmail.com>
2026-08-08 00:24:04 +0300

    feat(next.config): configure TypeScript settings and output mode for production build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	// typedRoutes: true,
+	output: "standalone",
+	// Skip TypeScript type-checking during production build
+	typescript: {
+		ignoreBuildErrors: true,
+	},
 	/* config options here */
 	allowedDevOrigins: ["*.*.*.*"],
 	async rewrites() {
