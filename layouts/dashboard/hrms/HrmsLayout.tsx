df19c35407c0333947837d4880857fdb63d123ab
mr-piyson <mun.ateeq@gmail.com>
2026-08-23 20:55:27 +0300

    refactor: remove ShiftAssignmentsPage, TrainingListPage, and TrainingDetailPage components

--- layouts/dashboard/hrms/HrmsLayout.tsx
+++
@@ -1,9 +0,0 @@
-import { DialogsProvider } from "@/components/dialogs";
-
-export default function HrmsLayout({
-	children,
-}: {
-	children: React.ReactNode;
-}) {
-	return <DialogsProvider>{children}</DialogsProvider>;
-}
