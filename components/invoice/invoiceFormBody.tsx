ae27b28535d2a23b766f47275b2ebe72ba32cc2c
mr-piyson <mun.ateeq@gmail.com>
2026-09-13 22:14:14 +0300

    fix(invoice): remove unused ArrowLeft import from invoiceFormBody

--- components/invoice/invoiceFormBody.tsx
+++ components/invoice/invoiceFormBody.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ArrowLeft, Package, Pencil, Plus, Trash2, Wrench } from "lucide-react";
+import { Package, Pencil, Plus, Trash2, Wrench } from "lucide-react";
 import { useTranslations } from "next-intl";
 import type * as React from "react";
 import { InvoiceLineDialog } from "@/components/dialogs/invoiceLineDialog";
